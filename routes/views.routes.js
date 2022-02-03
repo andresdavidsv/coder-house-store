3c8140b974db5834741d75ef785dbafc6d4d3e75
andresdavidsv <andresdavidsolartevidal@gmail.com>
2022-02-02 21:20:07 -0500

    feat: docker file nginx server

--- routes/views.routes.js
+++ routes/views.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { config } = require('../config');
 
 //controller
 const viewsController = require('../controllers/viewsControllers');
@@ -24,6 +25,18 @@ function viewsRoute(app) {
   router.get('/info', viewsController.viewInfo);
 
   router.get('/api/randoms', viewsController.viewNumberRandom);
+
+  router.get('/datos', (_request, response) => {
+    console.log(`PORT: ${config.dbPort} --> FYH: ${Date.now()}`);
+
+    return response
+      .status(200)
+      .send(
+        `Servidor Express <span style="color: blueviolet;">(Nginx)</span> en ${config.dbPort} - <b>PID ${
+          process.pid
+        }</b> - ${new Date().toLocaleString()}`
+      );
+  });
 }
 
 module.exports = viewsRoute;
